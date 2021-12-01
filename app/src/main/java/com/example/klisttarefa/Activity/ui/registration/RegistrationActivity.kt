b6b098c9690aa087df4b41556f1969cf487c8844
renan <renan_carpinelli@hotmail.com>
2021-12-01 20:57:20 -0300

    Adiciona registration state

--- app/src/main/java/com/example/klisttarefa/Activity/ui/registration/RegistrationActivity.kt
+++ app/src/main/java/com/example/klisttarefa/Activity/ui/registration/RegistrationActivity.kt
@@ -1,18 +1,17 @@
 package com.example.klisttarefa.Activity.ui.registration
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.example.klisttarefa.Activity.repository.dataBase.RegistrationDataBase
-import com.example.klisttarefa.Activity.repository.model.Registration
-import com.example.klisttarefa.Activity.repository.repository.RegistrationRepository
 import com.example.klisttarefa.R
 import com.example.klisttarefa.databinding.ActivityRegistrationBinding
 import kotlinx.coroutines.launch
 
 class RegistrationActivity : AppCompatActivity() {
+
+    lateinit var dataBase: RegistrationDataBase
 
     lateinit var viewModel: RegistrationViewModel
     private val binding by lazy { ActivityRegistrationBinding.inflate(layoutInflater) }
@@ -22,30 +21,41 @@ class RegistrationActivity : AppCompatActivity() {
         setContentView(R.layout.activity_registration)
         setContentView(binding.root)
 
-        viewModel = RegistrationViewModel()
-        viewModel.initViewModel(this)
+        dataBase = RegistrationDataBase.getInstance(this)
+        viewModel = RegistrationViewModel(dataBase)
 
-        binding.btnSalvar.setOnClickListener {
+        setupListeners()
+        setupObservers()
+    }
+
+    private fun setupListeners() = with(binding) {
+        btnSalvar.setOnClickListener {
             lifecycleScope.launch { addRegistry() }
         }
     }
 
-    suspend fun addRegistry() {
-        try {
-            if (binding.edtActivity.text.isNotEmpty() && binding.edtType.text.isNotEmpty()) {
-                val registration = Registration(
-                    activity = binding.edtActivity.text.toString(),
-                    type = binding.edtType.text.toString(),
-                    check = false
-                )
-
-                viewModel.addRegistry(registration)
-                finish()
-            } else {
-                Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_LONG).show()
+    private fun setupObservers() {
+        viewModel.registrationState.observe(this, { registration ->
+            when (registration) {
+                RegistrationViewModel.RegistrationState.Success -> finish()
+                RegistrationViewModel.RegistrationState.Error -> showError()
+                RegistrationViewModel.RegistrationState.Empty-> showEmptyError()
             }
-        } catch (e: Exception) {
-            Log.e("Erro", "Mensagem: " + e)
-        }
+        })
+    }
+
+    private suspend fun addRegistry() = with(binding) {
+        viewModel.addRegistry(
+            text = edtActivity.text.toString(),
+            type = edtType.text.toString()
+        )
+    }
+
+    private fun showError() {
+        Toast.makeText(this, "Erro ao salvar no banco", Toast.LENGTH_LONG).show()
+    }
+
+    private fun showEmptyError() {
+        Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_LONG).show()
     }
 }
--- app/src/main/java/com/example/klisttarefa/Activity/ui/registration/RegistrationViewModel.kt
+++ app/src/main/java/com/example/klisttarefa/Activity/ui/registration/RegistrationViewModel.kt
@@ -1,22 +1,48 @@
 package com.example.klisttarefa.Activity.ui.registration
 
-import android.content.Context
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.example.klisttarefa.Activity.repository.dataBase.RegistrationDataBase
 import com.example.klisttarefa.Activity.repository.model.Registration
 import com.example.klisttarefa.Activity.repository.repository.RegistrationRepository
 
-class RegistrationViewModel {
+class RegistrationViewModel(
+    dataBase: RegistrationDataBase
+) : ViewModel() {
 
-    lateinit var registrationRepository: RegistrationRepository
+    private val _registrationState = MutableLiveData<RegistrationState>()
+    val registrationState = _registrationState
 
-    fun initViewModel(context: Context) {
-        registrationRepository = RegistrationRepository(RegistrationDataBase.getInstance(context).registrationDao)
+    var registrationRepository = RegistrationRepository(dataBase.registrationDao)
+
+    suspend fun addRegistry(text: String, type: String) {
+        if (text.isNotEmpty() && type.isNotEmpty()) {
+            val registration = Registration(
+                activity = text,
+                type = type,
+                check = false
+            )
+            Log.e("add", "Add Registro: " + registration.toString())
+            saveRegistration(registration)
+        } else {
+            _registrationState.value = RegistrationState.Empty
+        }
     }
 
-    suspend fun addRegistry(registration: Registration) {
-        registrationRepository.save(registration)
-        Log.e("add", "Add Registro: " + registration.toString())
+    private suspend fun saveRegistration(registration: Registration) {
+        try {
+            registrationRepository.save(registration)
+            _registrationState.value = RegistrationState.Success
+        } catch (e: Exception) {
+            Log.e("Erro", "Mensagem: " + e)
+            _registrationState.value = RegistrationState.Error
+        }
     }
 
+    sealed class RegistrationState {
+        object Success : RegistrationState()
+        object Error : RegistrationState()
+        object Empty : RegistrationState()
+    }
 }
